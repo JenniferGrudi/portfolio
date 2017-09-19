c19aa4d30a1cd51156c5d24341b7fb56b098e47f
jennifergrudi <jen.grudi@gmail.com>
2017-09-19 09:53:45 -0400

    added cicleCi folder and config file in order to set up timbersafe ci testing

--- app.rb
+++ app.rb
@@ -4,14 +4,6 @@ get '/' do
  erb :index 
 end  
 
-get '/resume' do
-  erb :resume
-end  
-
-post '/resume' do
-  erb :resume
-end
-
 get '/blog' do
   erb :blog
 end
--- test_setup.rb
+++ test_setup.rb
@@ -1,4 +1,4 @@
-require 'selenuim-webdriver'
+require 'selenium-webdriver'
 require 'test/unit'
 
 class ExampleTestCase < Test::Unit::TestCase
@@ -12,6 +12,13 @@ class ExampleTestCase < Test::Unit::TestCase
   def teardown
     @driver.close
   end
+
+  def test_skills_button
+    @driver.find_element(:xpath, '//*[@id="skills_id"]').click()
+
+    skills = @driver.find_element(:xpath, '//*[@id="skills"]/div[2]/div/div/ul').text.include?('HTML')
+    assert_equal(true, skills, "Error, no skills are present")
+  end  
 
   
 end  
