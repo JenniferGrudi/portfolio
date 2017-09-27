394821c0c371b0a99037977ab16d8e21c2f25d81
jennifergrudi <jen.grudi@gmail.com>
2017-09-26 21:39:11 -0400

    added a test to check URL

--- test/test_setup.rb
+++ test/test_setup.rb
@@ -5,7 +5,7 @@ class ExampleTestCase < Test::Unit::TestCase
 
   def setup
   caps = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: { args: [ "--headless" ]})  
-  @driver = Selenium::WebDriver.for:chrome 
+  @driver = Selenium::WebDriver.for:chrome
   #, desired_capabilities:caps
   @driver.manage.window.maximize()
   @driver.navigate.to("https://all-about-me.herokuapp.com/")
@@ -15,12 +15,16 @@ class ExampleTestCase < Test::Unit::TestCase
     @driver.close
   end
 
+  def test_current_url
+    actualURL = @driver.current_url
+    assert_equal("https://all-about-me.herokuapp.com/", actualURL, "URL Did not match")
+  end
+
   def test_skills_button
     @driver.find_element(:xpath, '//*[@id="skills_id"]').click()
 
     skills = @driver.find_element(:xpath, '//*[@id="skills"]/div[2]/div/div/ul').text.include?('HTML')
     assert_equal(true, skills, "Error, no skills are present")
   end  
-
   
 end  
